e85723e601da96941f90f15e89a701a717c9ff8a
gabrisousa <gabrielsousaferreira186@gmail.com>
2025-02-24 14:47:33 -0300

    commit

--- src/js/responsivo.js
+++ src/js/responsivo.js
@@ -5,7 +5,7 @@ function fotoSobre() {
       fotoSobre.src = "./src/imagens/about_bg-about-sp.jpg";
 
     } else {
-      video.src = "./src/imagens/fundo-sobre.jp";
+      video.src = "./src/imagens/fundo-sobre.jpg";
     }
   }
 
