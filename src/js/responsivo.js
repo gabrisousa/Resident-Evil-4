99813b8f0181b2b0518a828c15942eb09df2ee35
gabrisousa <gabrielsousaferreira186@gmail.com>
2025-03-09 01:47:26 -0300

    ajustes

--- src/js/responsivo.js
+++ src/js/responsivo.js
@@ -14,33 +14,33 @@ window.addEventListener("load", fotoSobre);
 
 
 
-// function fotoHistoria() {
-//   let fotoHistoria = document.querySelector(".historia");
+function fotoHistoria() {
+  let fotoHistoria = document.querySelector(".historia");
   
-//   if (window.innerWidth < 1000) {
-//     fotoHistoria.src = "./src/imagens/about_bg-story-sp.jpg";
+  if (window.innerWidth < 1000) {
+    fotoHistoria.src = "./src/imagens/about_bg-story-sp.jpg";
 
-//   } else {
-//     fotoHistoria.src = "./src/imagens/history.png";
-//   }
-// }
+  } else {
+    fotoHistoria.src = "./src/imagens/history.png";
+  }
+}
 
-// window.addEventListener("resize", fotoHistoria);
-// window.addEventListener("load", fotoHistoria);
+window.addEventListener("resize", fotoHistoria);
+window.addEventListener("load", fotoHistoria);
 
 
 
-// function fotoLeonBio() {
-//   let fotoLeonBio = document.getElementById("image1");
+function fotoLeonBio() {
+  let fotoLeonBio = document.getElementById("image1");
   
-//   if (window.innerWidth < 1000) {
-//     fotoLeonBio.src = "./src/imagens/leon-biography-responsivo.jpg";
+  if (window.innerWidth < 1000) {
+    fotoLeonBio.src = "./src/imagens/leon-biography-responsivo.jpg";
 
-//   } else {
-//     fotoLeonBio.src = "./src/imagens/leon-biography.jpg";
-//   }
-// }
+  } else {
+    fotoLeonBio.src = "./src/imagens/leon-biography.jpg";
+  }
+}
 
-// window.addEventListener("resize", fotoLeonBio);
-// window.addEventListener("load", fotoLeonBio);
+window.addEventListener("resize", fotoLeonBio);
+window.addEventListener("load", fotoLeonBio);
 
